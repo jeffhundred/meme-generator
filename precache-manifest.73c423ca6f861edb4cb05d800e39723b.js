4712b6249376a53a8c75673a327fe32e8c1b564d
Ben Boyer <benboyer128@gmail.com>
2020-01-20 00:18:39 -0500

    Updates

---
+++ precache-manifest.73c423ca6f861edb4cb05d800e39723b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2917a0c243a84f04256fa68eae5a18",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "8a5e9ccecd76a8a5d8f4",
+    "url": "/meme-generator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa7d07319f00628d4fc6",
+    "url": "/meme-generator/static/js/2.75df78d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.75df78d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a5e9ccecd76a8a5d8f4",
+    "url": "/meme-generator/static/js/main.a0fde3d6.chunk.js"
+  },
+  {
+    "revision": "598190edf62d82c31ab5",
+    "url": "/meme-generator/static/js/runtime-main.90af1bc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meme-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
